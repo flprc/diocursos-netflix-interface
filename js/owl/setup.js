067d2f8c24d96aadf9f9a2c38497ec15e6fdcaa5
Felipe Cavalcante <103554466+flprc@users.noreply.github.com>
2022-12-07 21:51:10 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+        1200:{
+            items:5
+        }
+
+    }
+})
